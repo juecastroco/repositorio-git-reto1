ab42dd7e7529913f2b9683a8de11b0aedac5370f
juecastroco <juecastroco11@hotmail.com>
2022-10-05 19:56:22 -0500

    Agregación archivo Final.js y nuevas modificaciones al archivo Hola.js

--- Hola.js
+++ Hola.js
@@ -1,2 +1,4 @@
 
 console.log("Hola, soy el archivo Hola.js") 
+console.log("Soy la primera modificaci¢n del archivo Hola.js") 
+console.log("Soy una modificaci¢n al archivo original") 
---
+++ Mundo.js
@@ -0,0 +1,2 @@
+
+console.log("Hola, soy el archivo Mundo.js") 
